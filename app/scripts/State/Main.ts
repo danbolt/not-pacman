861f94f04e4b2e2b15f35e2058fa0ddf1bc3d614
Daniel Savage <dansvg@gmail.com>
2014-10-03 22:32:51 -0700

    Renders a tilemap now

--- app/scripts/State/Main.ts
+++ app/scripts/State/Main.ts
@@ -1,9 +1,20 @@
-module Pacman.State {
-  export class Main extends Phaser.State {
-    create() {
-      this.stage.backgroundColor = 0x000000;
-      
-      // Create game objects here
+module Pacman.State
+{
+  export class Main extends Phaser.State
+  {
+    private map:Phaser.Tilemap;
+    private layer:Phaser.TilemapLayer;
+
+    create()
+    {
+      this.stage.backgroundColor = 0xFF00FF;
+
+      this.map = this.game.add.tilemap('pacman-maze');
+
+      this.map.addTilesetImage('maze', 'pacman-sheet');
+      this.layer = this.map.createLayer('background');
+      this.layer = this.map.createLayer('maze');
+      this.layer.resizeWorld();
     }
   }
 }
--- app/scripts/State/Preload.ts
+++ app/scripts/State/Preload.ts
@@ -6,13 +6,16 @@ module Pacman.State {
       this.preloadBar = this.add.sprite(0, 148, 'preload-bar');
       this.load.setPreloadSprite(this.preloadBar);
 
+      this.load.tilemap('pacman-maze', 'assets/maps/maze.json', null, Phaser.Tilemap.TILED_JSON);
+
       this.load.image('menu-background', 'assets/images/menu-background.png');
-      
+      this.load.image('pacman-sheet', 'assets/images/pacman.png');
+
       // Load remaining assets here
     }
 
     create() {
-      this.game.state.start('menu');
+      this.game.state.start('main');
     }
   }
 }
